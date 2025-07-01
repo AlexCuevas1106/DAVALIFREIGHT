881fe819447c7dcabc691d713a1e55429fe7fe15
Al3jandro11 <131829043+Al3jandro11@users.noreply.github.com>
2025-07-01 21:26:02 +0000

    Restored to '50f420cdbb47d46c7f0e1fe962f7c7260503cc92'
    Replit-Restored-To: 50f420cdbb47d46c7f0e1fe962f7c7260503cc92

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import App from "./App.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-// Import TomTom CSS
-import '@tomtom-international/web-sdk-maps/dist/maps.css';
 
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <Toaster />
-    </QueryClientProvider>
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
